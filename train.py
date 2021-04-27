3f5511fd45db2265e9e26f544dd09ea46d1ac831
Agchai52 <shuangzhang0809@gmail.com>
2021-04-26 21:05:28 -0400

    Fix: bug

--- train.py
+++ train.py
@@ -109,7 +109,7 @@ def train(args):
 
             roi_B_interp = F.interpolate(roi_B, (args.load_size, args.load_size), mode="bilinear")
 
-            roi_B_real = torch.where(rea_B_ > -0.4, real_B_, -1.0)
+            roi_B_real = torch.where(real_B_ > -0.4, real_B_, -1.0)
             roi_B_real = torch.where(roi_B_real <= -0.4, roi_B_real, 1.0)
             loss_roi = criterion_L2(roi_B_interp, roi_B_real)
 
