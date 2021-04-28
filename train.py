995b2cdd5f60c05de57eaec9bfce88f4b65740f6
Agchai52 <shuangzhang0809@gmail.com>
2021-04-28 13:44:19 -0400

    Fix: bugs

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ def train(args):
             ###########################
             optimizer_G.zero_grad()
 
-            real_B = F.interpolate(real_B, (args.load_szie, args.load_size), mode="bilinear")
+            real_B = F.interpolate(real_B, (args.load_size, args.load_size), mode="bilinear")
 
             loss_l2 = criterion_L2(fake_S, real_S) * args.L2_lambda
             loss_grad = criterion_grad(fake_S, real_S) * args.L2_lambda
