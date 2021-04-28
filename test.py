4864e8bcf02324ae4c27bdb5bca6eeee769a3c6a
Agchai52 <shuangzhang0809@gmail.com>
2021-04-28 19:59:05 -0400

    Refactor: post_interp -> convT

--- test.py
+++ test.py
@@ -63,7 +63,7 @@ def test(args):
             real_B, real_S = real_B.to(device), real_S.to(device)  # B = (B, 1, 64, 64), S = (B, 1, 256, 256)
 
             pred_S = netG(real_B)
-            pred_S = F.interpolate(pred_S, (args.load_size, args.load_size), mode='bilinear')
+            # pred_S = F.interpolate(pred_S, (args.load_size, args.load_size), mode='bilinear')
             cur_psnr, cur_ssim = compute_metrics(real_S, pred_S)
             all_psnr.append(cur_psnr)
             all_ssim.append(cur_ssim)
@@ -117,7 +117,7 @@ def test_real(args):
             real_B = real_B.to(device)
 
             pred_S = netG(real_B)
-            pred_S = F.interpolate(pred_S, (args.load_size, args.load_size), mode='bilinear')
+            # pred_S = F.interpolate(pred_S, (args.load_size, args.load_size), mode='bilinear')
             img_S = pred_S.detach().squeeze(0).cpu()
             save_img(img_S, '{}/real_'.format(args.test_dir) + img_name[0])
 
