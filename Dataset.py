7039e483d585cebd9d41d0d879497ce22abb02a0
Agchai52 <shuangzhang0809@gmail.com>
2021-04-30 22:18:16 -0400

    Fix: bugs

--- Dataset.py
+++ Dataset.py
@@ -26,7 +26,6 @@ class DeblurDataset(Dataset):
         img_path, label = line.split('\t')
         label = np.array([float(label)])
         label = np.expand_dims(label, axis=0)
-        label = torch.from_numpy(label)
 
         img_A = Image.open(img_path + '_blur.png').convert('L')
         img_B = Image.open(img_path + '_sharp.png').convert('L')
