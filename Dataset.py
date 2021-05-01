d6481b851e2acd2e905ee37fc088c6e18db80227
Agchai52 <shuangzhang0809@gmail.com>
2021-05-01 00:27:54 -0400

    Fix: bugs

--- Dataset.py
+++ Dataset.py
@@ -24,7 +24,7 @@ class DeblurDataset(Dataset):
 
         line = self.img_path[index]
         img_path, label = line.split('\t')
-        label = np.array([np.float(label)])
+        label = np.array(np.float(label))
         label = np.expand_dims(label, axis=0)
 
         img_A = Image.open(img_path + '_blur.png').convert('L')
